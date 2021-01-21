5fbf4ff17b3b8a7be043fafd8aa20cca8cc549ce
Joe Previte <jjprevite@gmail.com>
2021-01-21 12:18:04 -0700

    wip

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "lib/vscode/"],
+  testPathIgnorePatterns: ["node_modules/", "lib/vscode/"],
+  roots: ["../"],
+  collectCoverage: true,
+  collectCoverageFrom: ["../src/**/*.ts"],
+  coverageDirectory: "./coverage",
+  coverageReporters: ["json", "text"],
 }
